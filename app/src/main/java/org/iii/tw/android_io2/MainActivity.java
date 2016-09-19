2752c1cd1f01878f2878135e6086fea7c3dec70d
chuan1983 <chuan1983126@gmail.com>
2016-09-19 14:13:23 +0800

    v2

--- app/src/main/java/org/iii/tw/android_io2/MainActivity.java
+++ app/src/main/java/org/iii/tw/android_io2/MainActivity.java
@@ -1,13 +1,28 @@
 package org.iii.tw.android_io2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("brad", "ok");
+            }else{
+                Log.d("brad", "XX");
+            }
+        }
     }
+
 }
